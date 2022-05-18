9dfbfbe8682f86fb3d7833bbf9d5a6b75e2be1fc
Alan <mejia.alan.r@gmail.com>
2022-05-18 11:32:07 -0300

    Project Init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
